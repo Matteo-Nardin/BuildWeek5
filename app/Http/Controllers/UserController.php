5ac46435a87a1f99c12d32df9b9f1391fe0b59ac
Vincenzo Saccone <143630974+VinsWhite@users.noreply.github.com>
2024-03-26 11:18:26 +0100

    gestione utente

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -12,8 +14,13 @@ class UserController extends Controller
      */
     public function index()
     {
+        $user = Auth::user();
+        if($user->user_role == 'admin') {
         $userList = User::paginate(8);
         return view('users' , ['userList' => $userList]);
+        } else {
+            return redirect()->back();
+        }
     }
 
     /**
@@ -21,7 +28,15 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+
+        $user = Auth::user();
+
+        if($user->user_role == 'admin') {
+            return view('create_user');
+        } else {
+            return redirect()->back();
+        }
+        
     }
 
     /**
@@ -29,7 +44,16 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $userData = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'user_role' => $request->user_role
+        ];
+
+        User::create($userData);    
+        
+        return redirect()->action([UserController::class, 'index']);
     }
 
     /**
@@ -51,16 +75,33 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, User $user)
     {
-        //
+
+        $user = User::findOrFail($request->user_id);
+
+        if($user->user_role == 'admin') {
+            $user->user_role = 'user';
+            $user->save();
+
+        } elseif($user->user_role == 'user') {
+            $user->user_role = 'admin';
+            $user->save();
+        }
+
+        return redirect()->back()->with('success');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(User $user)
     {
-        //
+        if (Auth::user()->user_role === 'admin') {
+            $user->delete();
+            return redirect()->back()->with('success');
+        } else {
+            return redirect()->back()->with('error', 'You do not have permission to delete this reservation.');
+        }
     }
 }
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -9,6 +9,8 @@
     <main class="container mt-5 shadow p-3 bg-white">
         <h2 class="h2">Gestisci utenti!</h2>
 
+        <a href="/user/create" class="btn btn-primary">Aggiungi utente</a>
+
         <table class="table">
             <thead>
                 <tr>
@@ -20,12 +22,42 @@
                 </tr>
             </thead>
             <tbody>
+                @php
+                    $adminCount = $userList->where('user_role', 'admin')->count();
+                @endphp
+
                 @foreach ($userList as $user)
                     <tr>
                         <th scope="row">{{ $loop->iteration }}</th>
                         <td>{{$user->name}}</td>
                         <td>{{$user->email}}</td>
                         <td>{{$user->user_role}}</td>
+                        <td class="">
+                            @if($user->user_role == "admin")
+                                @if($adminCount > 1)
+                                <form method="POST" action="{{ route('user.update', ['user' => $user->id]) }}">
+                                    @csrf
+                                    @method('PATCH')
+                                    <input type="hidden" name="user_id" value="{{ $user->id }}">
+                                    <button type="submit" class="btn btn-danger">Rendi utente normale</button>
+                                </form>
+                                @endif
+                            @elseif($user->user_role == "user")
+                                <div class="d-flex">
+                                    <form method="POST" action="{{ route('user.update', ['user' => $user->id]) }}">
+                                        @csrf
+                                        @method('PATCH')
+                                        <input type="hidden" name="user_id" value="{{ $user->id }}">
+                                        <button type="submit" class="btn btn-primary">Rendi admin</button>
+                                    </form>
+                                    <form method="POST" action="{{ route('user.destroy', ['user' => $user->id]) }}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger ms-5"><i class="bi bi-trash text-light"></i></button>
+                                    </form>
+                                </div>
+                            @endif
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
